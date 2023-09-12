cebb64e68ae30576b896764995431e2386afd75b
BrianCastro2022 <112706633+BrianCastro2022@users.noreply.github.com>
2023-09-12 11:24:04 -0500

    Add files via upload
    Subido

---
+++ src/main/java/com/umariana/mundo/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package com.umariana.mundo;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author BRIAN
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
